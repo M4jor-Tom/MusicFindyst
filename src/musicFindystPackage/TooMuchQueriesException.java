f062f01231f70bc65906cb42082e60ad5f1e7a6f
Tom <tom-vautray@hotmail.fr>
2021-05-02 16:06:21 +0200

    Set of TooMuchQueriesException.getMessage()

--- src/musicFindystPackage/TooMuchQueriesException.java
+++ src/musicFindystPackage/TooMuchQueriesException.java
@@ -6,4 +6,10 @@ public class TooMuchQueriesException extends Exception
 	 * 
 	 */
 	private static final long serialVersionUID = -6119185287638500993L;
+
+	@Override
+	public String getMessage()
+	{
+		return "Too much queries sent, please wait an hour or change your Ip";
+	}
 }
